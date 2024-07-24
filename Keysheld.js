ca3fa99a2c5cf1ac4bfddf1529a7799b54472bd0
LEGEND <legendboss123gaming@gmail.com>
2024-07-24 15:11:44 -0700

    Add files via upload

---
+++ Keysheld.js
@@ -0,0 +1,17 @@
+var Keysheld = class {
+
+    constructor(element) {
+        this.keysheld = {};
+
+        element.addEventListener('keydown', function(e){
+            this.keysheld[e.code] = true;
+        }.bind(this));
+        element.addEventListener('keyup', function(e){
+            this.keysheld[e.code] = false;
+        }.bind(this));
+    }
+
+    isHeld(key) {
+        return this.keysheld[key] || false;
+    }
+}
